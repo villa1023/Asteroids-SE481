6d51acceee1f77fb0ad239674ad5da7c0679a27f
Cam Murriel <50788490+cammurriel@users.noreply.github.com>
2021-12-06 23:20:52 -0800

    testing bad test from method testAddScore()

---
+++ asteroids/src/test/java/com/csusm/villa291/GameTest.java
@@ -0,0 +1,59 @@
+package com.csusm.villa291;
+
+import junit.framework.TestCase;
+
+public class GameTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+        GameTest gameTest = new GameTest();
+    }
+
+
+
+    public void testKillPlayer() {
+
+    }
+
+    public void testAddScore() {
+        assertEquals(-1,1);
+    }
+
+    public void testRegisterEntity() {
+    }
+
+    public void testIsGameOver() {
+    }
+
+    public void testIsPlayerInvulnerable() {
+    }
+
+    public void testCanDrawPlayer() {
+    }
+
+    public void testGetScore() {
+    }
+
+    public void testGetLives() {
+    }
+
+    public void testGetLevel() {
+    }
+
+    public void testIsPaused() {
+    }
+
+    public void testIsShowingLevel() {
+    }
+
+    public void testGetRandom() {
+    }
+
+    public void testGetEntities() {
+    }
+
+    public void testGetPlayer() {
+    }
+    public void tearDown() throws Exception {
+    }
+}
